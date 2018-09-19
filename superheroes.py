7e41537f78bb73f14ecf97ef3d36d258fb003d19
Jayce Azua <jayce.azua@gmail.com>
2018-09-19 11:44:09 -0700

    ...

--- superheroes.py
+++ superheroes.py
@@ -1,74 +1,42 @@
 import random
 
 class Ability:
-    def __init__(self, name, attack_strength): # Initalize starting values
-        # Set Ability name
+    def __init__(self, name, attack_strength):
+        # TODO: Set Ability name
         self.name = name
-        # Set attack strength
+        # TODO: Set attack strength
         self.attack_strength = attack_strength
 
-    def attack(self): # Return attack value
-        # Calculate lowest attack value as an integer
-        full_attack = 20
-        lowest_attack = full_attack // 2
-        # Use random.randit(a, b) to select a random attack value.
-        attack_value = random(lowest_attack, full_attack)
-        # Return attack value between 0 and the full attack
-        return attack_value
-
-
-    def update_attack(self, attack_strength): # Update attack value
-        attack_value = attack_strength
-
-
-class Weapon(Ability):
-    
     def attack(self):
-        """
-        This method should should return a random value
-        between 0 and the full attack power of the weapon.
-        Hint: The attack power is inherited.
-        """
+        # TODO: Calculate lowest attack value as an integer.
+        # TODO: Use random.randint(a, b) to select a random attack value.
+        # TODO: Return attack value between 0 and the full attack.
+        return random.randint(self.attack_strength // 2 , self.attack_strength)
 
 
-class Team:
-    def init(self, team_name):
-        """Instantiate resources."""
-        self.name = team_name
-        self.heroes = list()
 
-    def add_hero(self, Hero):
-        """Add Hero object to heroes list."""
+    def update_attack(self, attack_strength):
+        # TODO: update the value of the current attack strength with the new value passed in as a parameter.
+        self.attack_strength = attack_strength
 
-    def remove_hero(self, name):
-        """
-        Remove hero from heroes list.
-        If Hero isn't found return 0.
-        """
-
-    def find_hero(self, name):
-        """
-        Find and return hero from heroes list.
-        If Hero isn't found return 0.
-        """
-
-    def view_all_heroes(self):
-        """Print out all heroes to the console."""
 
 class Hero:
     def __init__(self, name):
-        # Initialize starting values
-        self.abilities = list() self.name = name
+        self.abilities = list()
+        self.name = name
 
     def add_ability(self, ability):
-        # Add ability to abilities list
-        self.abilities.append(ability)
+        # Append ability to self.abilities
+        self.ability.append(ability)
 
-    def attack(self): # Run attack() on every ability hero has
+    def attack(self):
         # Call the attack method on every ability in our ability list
+        for ability in self.abilities:
         # Add up and return the total of all attacks
 
-if name == "__main__": # If you run this file from the terminal this block is executed.
+
+
+if __name__ == "__main":
     hero = Hero("Wonder Woman")
     print(hero.attack())
     ability = Ability("Divine Speed", 300)
