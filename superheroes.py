11d58fe76dfd99e49527270a2079e2b7a6cef24b
Jayce Azua <jayce.azua@gmail.com>
2018-09-20 20:23:41 -0700

    got it working

--- superheroes.py
+++ superheroes.py
@@ -2,21 +2,13 @@ import random
 
 class Ability:
     def __init__(self, name, attack_strength):
-        # TODO: Set Ability name
         self.name = name
-        # TODO: Set attack strength
         self.attack_strength = attack_strength
 
     def attack(self):
-        # TODO: Calculate lowest attack value as an integer.
-        # TODO: Use random.randint(a, b) to select a random attack value.
-        # TODO: Return attack value between 0 and the full attack.
         return random.randint(self.attack_strength // 2 , self.attack_strength)
 
-
-
     def update_attack(self, attack_strength):
-        # TODO: update the value of the current attack strength with the new value passed in as a parameter.
         self.attack_strength = attack_strength
 
 
@@ -26,16 +18,57 @@ class Hero:
         self.name = name
 
     def add_ability(self, ability):
-        # Append ability to self.abilities
         self.abilities.append(ability)
 
     def attack(self):
         power = 0
-        # Call the attack method on every ability in our ability list
         for ability in self.abilities:
-        # Add up and return the total of all attacks
             power += ability.attack()
         return power
+
+class Weapon(Ability):
+    def attack(self):
+        """
+        This method should should return a random value
+        between 0 and the full attack power of the weapon.
+        Hint: The attack power is inherited.
+        """
+        return random.randint(0, self.attack_strength)
+
+class Team:
+    def __init__(self, team_name):
+        """Instantiate resources."""
+        self.name = team_name
+        self.heroes = list()
+
+    def add_hero(self, Hero):
+        """Add Hero object to heroes list."""
+        self.heroes.append(Hero)
+
+    def remove_hero(self, name):
+        """
+        Remove hero from heroes list.
+        If Hero isn't found return 0.
+        """
+        pass
+
+
+    def find_hero(self, name):
+        """
+        Find and return hero from heroes list.
+        If Hero isn't found return 0.
+        """
+        for hero in self.heroes:
+            if hero.name == name:
+                return hero.name
+        return "{} is not found in the {} team.".format(name, self.name)
+
+
+
+    def view_all_heroes(self):
+        """Print out all heroes to the console."""
+        for hero  in self.heroes:
+            print(hero.name)
 
 
 if __name__ == "__main__":
@@ -47,3 +80,7 @@ if __name__ == "__main__":
     new_ability = Ability("Super Human Strength", 800)
     hero.add_ability(new_ability)
     print(hero.attack())
+    team = Team("Justice")
+    team.add_hero(hero)
+    team.view_all_heroes()
+    print(team.find_hero("Batman"))
