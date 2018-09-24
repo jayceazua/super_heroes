468bae22ede02e0213e9f05ff4ba4061b404840a
Jayce Azua <jayce.azua@gmail.com>
2018-09-24 15:44:49 -0700

    game loop

--- superheroes.py
+++ superheroes.py
@@ -371,6 +371,25 @@ class Arena:
         pass
 
 if __name__ == "__main__":
-    Infinity_War = Arena()
-    Infinity_War.build_team_one()
-    Infinity_War.build_team_two()
+
+    # Instantiate Game Arena
+    arena = Arena()
+
+    #Build Teams
+    arena.build_team_one()
+    arena.build_team_two()
+
+    while game_is_running:
+
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
+
+        #Check for Player Input
+        if play_again.lower() == "n":
+            game_is_running = False
+
+        else:
+            #Revive heroes to play again
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
