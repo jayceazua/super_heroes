848791f33738dba15c1afeaa5034fa02862878bd
Jayce Azua <jayce.azua@gmail.com>
2018-09-23 20:10:26 -0700

    ...

--- README.md
+++ README.md
@@ -6,12 +6,15 @@
 ### Description
 ...
 
-### To start game
+### To start the war
 ``` $ python3  ```
 
 ### Technologies used:
 - Python 3.7.0
 - Pytest 3.8.1
 
-## Future developments:
-- ✓ ...
+## Future developments / Stretch Challenges:
+- Add a command line interface that allows for creating, editing, and battling of teams.
+- Allow use of only "authorized" abilites, weapons, and armor controlled by the Arena.
+- Add tests that cover more edge cases.
+- Change the way health is dealt out across the team. i.e. create heroes that may take damage first or may take more of the teams damage.
--- superheroes.py
+++ superheroes.py
@@ -201,13 +201,116 @@ class Arena:
         self.team_one = None
         self.team_two = None
         """
+        self.team_one = None
+        self.team_two = None
 
-    def build_team_one(self):
+    def create_armor(self):
+        armors = [
+            "Calculator",
+            "Laser Shield",
+            "Invisibility",
+            "SFPD Strike Force",
+            "Social Workers",
+            "Face Paint",
+            "Damaskus Shield",
+            "Bamboo Wall",
+            "Forced Projection",
+            "Thick Fog",
+            "Wall of Will",
+            "Wall of Walls",
+            "Obamacare",
+            "Thick Goo"]
+        name = armors[random.randint(0, len(armors) - 1)]
+        power = random.randint(23, 700000)
+        return Armor(name, power)
+
+
+    def create_weapon(self):
+        weapons = [
+            "Antimatter Gun",
+            "Star Cannon",
+            "Black Hole Ram Jet",
+            "Laser Sword",
+            "Laser Cannon",
+            "Ion Accellerated Disc Drive",
+            "Superhuman Strength",
+            "Blinding Lights",
+            "Ferociousness",
+            "Speed of Hermes",
+            "Lightning Bolts"]
+        name = weapons[random.randint(0, len(weapons) - 1)]
+        power = random.randint(27, 700000)
+        return Weapon(name, power)
+
+
+    def create_ability(self):
+        abilities = [
+            "Alien Attack",
+            "Science",
+            "Star Power",
+            "Immortality",
+            "Grandmas Cookies",
+            "Blinding Strength",
+            "Cute Kittens",
+            "Team Morale",
+            "Luck",
+            "Obsequious Destruction",
+            "The Kraken",
+            "The Fire of A Million Suns",
+            "Team Spirit",
+            "Canada"]
+        name = abilities[random.randint(0, len(abilities) - 1)]
+        power = random.randint(45, 700000)
+        return Ability(name, power)
+
+    def create_hero(self, weapons=False, armors=False, health=False):
+        heroes = [
+            "Athena",
+            "Jodie Foster",
+            "Christina Aguilera",
+            "Gamora",
+            "Supergirl",
+            "Wonder Woman",
+            "Batgirl",
+            "Carmen Sandiego",
+            "Okoye",
+            "America Chavez",
+            "Cat Woman",
+            "White Canary",
+            "Nakia",
+            "Mera",
+            "Iris West",
+            "Quake",
+            "Wasp",
+            "Storm",
+            "Black Widow",
+            "San Luis Obispo",
+            "Ted Kennedy",
+            "San Francisco",
+            "Bananas"]
+        name = heroes[random.randint(0, len(heroes) - 1)]
+        if health:
+            power = health
+        else:
+            power = random.randint(3, 700000)
+        hero = Hero(name, power)
+        if weapons and armors:
+            for weapon in weapons:
+                hero.add_ability(weapon)
+            for armor in armors:
+                hero.add_armor(armor)
+        if armors and not weapons:
+            for armor in armors:
+                hero.add_armor(armor)
+        return hero
+
+    def build_team_one(self, heroes=[]):
         """
         This method should allow a user to build team one.
         """
+        self.team_one = Team()
 
-    def build_team_two(self):
+    def build_team_two(self, heroes=[]):
         """
         This method should allow user to build team two.
         """
@@ -226,12 +329,3 @@ class Arena:
 
 
 if __name__ == "__main__":
-    hero_1 = Hero("Wonder Woman")
-    hero = Hero("Batman")
-    print(hero.attack())
-    ability = Ability("Divine Speed", 300)
-    hero.add_ability(ability)
-    print(hero.attack())
-    new_ability = Ability("Super Human Strength", 800)
-    hero.add_ability(new_ability)
-    print(hero.attack())
